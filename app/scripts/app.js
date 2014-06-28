aaadb9e06cdd9837a77a07c04cb7bccaae6d8a83
Sebastian Popp <popping.net@gmail.com>
2014-06-28 18:40:29 -0300

    Agregado de animaciones de transicion entre paginas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,50 +8,51 @@
  *
  * Main module of the application.
  */
-angular
-    .module('guarinoApp', [
+var guarinoApp = angular.module('guarinoApp', [
         'ngAnimate',
         'ngCookies',
         'ngResource',
         'ngRoute',
         'ngSanitize',
         'ngTouch'
-    ])
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'views/home.html',
-                controller: 'HomeCtrl'
-            })
-            .when('/estudio', {
-                templateUrl: 'views/estudio.html',
-                controller: 'EstudioCtrl'
-            })
-            .when('/galeria', {
-              templateUrl: 'views/galeria.html',
-              controller: 'GaleriaCtrl'
-            })
-            .when('/procrear', {
-              templateUrl: 'views/procrear.html',
-              controller: 'ProcrearCtrl'
-            })
-            .when('/proyectos', {
-              templateUrl: 'views/proyectos.html',
-              controller: 'ProyectosCtrl'
-            })
-            .when('/proyectos/3d', {
-              templateUrl: 'views/3d.html',
-              controller: '3dCtrl'
-            })
-            .when('/proyectos/reciclado', {
-              templateUrl: 'views/reciclado.html',
-              controller: 'RecicladoCtrl'
-            })
-            .when('/contacto', {
-              templateUrl: 'views/contacto.html',
-              controller: 'ContactoCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+]);
+    
+guarinoApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'HomeCtrl'
+        })
+        .when('/estudio', {
+            templateUrl: 'views/estudio.html',
+            controller: 'EstudioCtrl'
+        })
+        .when('/galeria', {
+          templateUrl: 'views/galeria.html',
+          controller: 'GaleriaCtrl'
+        })
+        .when('/procrear', {
+          templateUrl: 'views/procrear.html',
+          controller: 'ProcrearCtrl'
+        })
+        .when('/proyectos', {
+          templateUrl: 'views/proyectos.html',
+          controller: 'ProyectosCtrl'
+        })
+        .when('/proyectos/3d', {
+          templateUrl: 'views/3d.html',
+          controller: '3dCtrl'
+        })
+        .when('/proyectos/reciclado', {
+          templateUrl: 'views/reciclado.html',
+          controller: 'RecicladoCtrl'
+        })
+        .when('/contacto', {
+          templateUrl: 'views/contacto.html',
+          controller: 'ContactoCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
